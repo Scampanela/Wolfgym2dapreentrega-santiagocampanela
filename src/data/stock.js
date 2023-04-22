44a573fc1dfde61b3bf5c0f9c0926f55766a577d
Santiago Campanela <scampanela@gmail.com>
2023-04-22 11:12:49 -0300

    FEATURE: Creamos boton de volver y hacemos funcionalidad el nuevo objeto

--- src/data/stock.js
+++ src/data/stock.js
@@ -5,42 +5,48 @@ export const stock = [
         description: "Pantalón",
         price: 35,
         image: "https://picsum.photos/200/300",
-        category: "ropa"
+        category: "ropa",
+        stock: 20
     },
     {
         id: 2,
         description: "Televisor",
         price: 352,
         image: "https://picsum.photos/id/237/200/300",
-        category: "electrodomestico"
+        category: "electrodomestico",
+        stock: 20
     },
     {
         id: 3,
         description: "Romero",
         price: 200,
         image: "https://picsum.photos/200/300?grayscale",
-        category: "ropa"
+        category: "ropa",
+        stock: 20
     },
     {
         id: 4,
         description: "Remera",
         price: 35,
         image: "https://picsum.photos/200/300",
-        category: "ropa"
+        category: "ropa",
+        stock: 20
     },
     {
         id: 5,
         description: "Celular",
         price: 352,
         image: "https://picsum.photos/id/237/200/300",
-        category: "electrodomestico"
+        category: "electrodomestico",
+        stock: 20
     },
     {
         id: 6,
         description: "Microondas",
         price: 200,
         image: "https://picsum.photos/200/300?grayscale",
-        category: "electrodomestico"
+        category: "electrodomestico",
+        stock: 20
     },
 
 ]
